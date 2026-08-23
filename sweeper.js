8323d39c9a3502292e6cb6497eb7fd87c633061f
thisstatementisalie <ferretswillrule@gmail.com>
2026-08-23 10:04:59 -0700

    really loving imagedata

--- sweeper.js
+++ sweeper.js
@@ -1,22 +1,17 @@
-var board;
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var boardData = ctx.createImageData(99, 99);
+
 var data;
 var size;
 var intBoard;
-var id;
+var boolBoard;
 
-function prepareThineSweeper(size, id) {
-    this.id = id;
-    this.size = size;
-    board=ImageData.createImageData(size, size);
-    blankBoard(id, size);
-    document.getElementById(id).putImageData(board, 0,0)
-}
-
-function blankBoard(id, size) {
-    data = board.data;
+function prepareThineSweeper() {
+    data = boardData.data;
     for (var x = 0; x < size*10+1; x++) {
         for (var y = 0; y < size*10+1; y++) {
-            if (x%10 == 0 || y%10 == 1) {
+            if (x%40 == 0 || y%40 == 1) {
                 data[4*(size*y+x)+0] = 0;
                 data[4*(size*y+x)+1] = 0;
                 data[4*(size*y+x)+2] = 0;
@@ -28,4 +23,5 @@ function blankBoard(id, size) {
             data[4*(size*y+x)+3] = 255;
         }
     }
+    ctx.putImageData(boardData,0,0);
 }
